ca6d0c683117c601b97525ebe875cf3e869325c2
Анатолий Еременко <yeremenkotolik@gmail.com>
2025-06-09 10:16:48 +0600

    Настроил PostgreSQL для Render

--- config/settings.py
+++ config/settings.py
@@ -76,9 +76,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True  # обязательно для Render
+        default=os.environ.get('DATABASE_URL')
     )
 }
 
