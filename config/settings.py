76710f78934b8102b736e250ea8ddd7f13a772aa
Анатолий Еременко <yeremenkotolik@gmail.com>
2025-06-09 10:41:16 +0600

    Настроил PostgreSQL для Render

--- config/settings.py
+++ config/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
